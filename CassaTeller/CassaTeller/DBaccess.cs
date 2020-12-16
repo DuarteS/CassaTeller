78876ef213b46e98ee6e0337e03d2f5eeb50433e
I450128 <d.somsen@student.fontys.nl>
2020-12-16 21:51:32 +0100

    Database setup, and connection created

---
+++ CassaTeller/CassaTeller/DBaccess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace CassaTeller
+{
+
+    class DBaccess
+    {
+        OleDbConnection con;
+
+        public DBaccess()
+        {
+            con = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source= CassaData.accdb");
+            OleDbCommand cmd = con.CreateCommand();
+            con.Open();
+            //cmd.CommandText = "INSERT INTO Cassa(`Date`,`Time`,`Worker`,`Total`,`InCassa`,`Description`) VALUES('17/12/2020','12:12','1','3465,12',False,'Test');";
+            //cmd.Connection = con;
+            //cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
